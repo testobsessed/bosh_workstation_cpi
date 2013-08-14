eacd4183d12ca3ee4ea2f53278e3576b67aa1119
Amit Gupta and Dmitriy Kalinin <pair+agupta+dmitriy@pivotallabs.com>
2013-08-14 09:05:56 -0700

    retry importing

--- lib/bosh_workstation_cpi/virtualbox/vm_importer.rb
+++ lib/bosh_workstation_cpi/virtualbox/vm_importer.rb
@@ -9,20 +9,38 @@ module BoshWorkstationCpi::Virtualbox
     end
 
     def import(ovf_path)
-      output = @driver.execute("import", ovf_path)
-      if output !~ /Suggested VM name "(.+?)"/
+      retry_times do
+        output = @driver.execute("import", ovf_path)
+        if output !~ /Suggested VM name "(.+?)"/
+          raise BoshWorkstationCpi::Virtualbox::Error, \
+            "Couldn't find VM name in the output"
+        end
+
+        name = $1.to_s
+        output = @driver.execute("list", "vms")
+        if output =~ /^"#{Regexp.escape(name)}" \{(.+?)\}$/
+          return Vm.new(@driver, $1.to_s, @logger)
+        end
+
         raise BoshWorkstationCpi::Virtualbox::Error, \
-          "Couldn't find VM name in the output"
+          "Failed to import #{ovf_path}"
       end
+    end
 
-      name = $1.to_s
-      output = @driver.execute("list", "vms")
-      if output =~ /^"#{Regexp.escape(name)}" \{(.+?)\}$/
-        return Vm.new(@driver, $1.to_s, @logger)
+    private
+
+    def retry_times(times=10, sleep=5, &blk)
+      blk.call
+    rescue BoshWorkstationCpi::Virtualbox::Error => e
+      times -= 1
+      if times.zero?
+        @logger.info("virtualbox.vm_importer.retry.failed error=#{e.inspect}")
+        raise
+      else
+        @logger.info("virtualbox.vm_importer.retry try=#{times}")
+        sleep(sleep)
+        retry
       end
-
-      raise BoshWorkstationCpi::Virtualbox::Error, \
-        "Failed to import #{ovf_path}"
     end
   end
 end
